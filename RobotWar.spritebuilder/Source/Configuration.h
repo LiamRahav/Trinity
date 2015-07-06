5fd4b5afde8052ee32b90ea286d9bd765d7be649
liamrahav <liorogamerdvd@gmail.com>
2015-07-06 10:40:15 -0400

    Trinity

--- RobotWar.spritebuilder/Source/Configuration.h
+++ RobotWar.spritebuilder/Source/Configuration.h
@@ -10,6 +10,6 @@
 #define RobotWar_Configuration_h
 
 static NSString *robotClass1 = @"SimpleRobot";
-static NSString *robotClass2 = @"CustomRobot";
+static NSString *robotClass2 = @"Trinity";
 
 #endif
---
+++ RobotWar.spritebuilder/Source/Robot/Trinity.swift
@@ -0,0 +1,13 @@
+//
+//  TurretRobotSwift.swift
+//  RobotWar
+//
+//  Created by Dion Larson on 7/2/15.
+//  Copyright (c) 2015 MakeGamesWithUs. All rights reserved.
+//
+
+import Foundation
+
+class Trinity: Robot {
+  
+}
