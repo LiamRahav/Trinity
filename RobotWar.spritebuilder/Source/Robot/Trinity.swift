f2e48ece9067a0715a57824d6b5f03bc1144f19f
liamrahav <liorogamerdvd@gmail.com>
2015-07-06 13:50:15 -0400

    Added abse functionality

--- RobotWar.spritebuilder/Source/Robot/Trinity.swift
+++ RobotWar.spritebuilder/Source/Robot/Trinity.swift
@@ -3,11 +3,41 @@ import Foundation
 
 class Trinity: Robot {
   
+  var lastKnownEnemyPosition = CGPoint(x: 0, y: 0)
+  var lastKnownEnemyTimestamp = CGFloat(0.0)
+  var state = State.Searching
+  
+  enum State {
+    case Searching, Detected, Firing, Chasing
+  }
+  
   override func run() {
     while true {
-      moveAhead(100)
-      turnRobotLeft(10)
+      switch state {
+        case .Searching:
+          search()
+        case .Detected:
+          println("Detected")
+        case .Firing:
+          println("Firing")
+        case .Chasing:
+          println("Chasing")
+      }
     }
+  }
+  
+  func search() {
+    // If we know where they were, go there
+    
+    // Else do random movement
+  }
+  
+  override func scannedRobot(robot: Robot!, atPosition position: CGPoint) {
+    lastKnownEnemyPosition = position
+    
+    turnRobotLeft(abs(Int(angleBetweenGunHeadingDirectionAndWorldPosition(position))))
+    turnGunLeft(abs(Int(angleBetweenGunHeadingDirectionAndWorldPosition(position))))
+    shoot()
   }
   
   override func hitWall(hitDirection: RobotWallHitDirection, hitAngle: CGFloat) {
